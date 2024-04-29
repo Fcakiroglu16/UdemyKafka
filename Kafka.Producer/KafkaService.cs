eb3b26bec01b50b0eecbc1cabdbefeed23bf7fb1
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-04-30 00:21:43 +0300

    How to create topic(queue)?

---
+++ Kafka.Producer/KafkaService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+namespace Kafka.Producer
+{
+    internal class KafkaService
+    {
+        private const string TopicName = "mytopic";
+
+
+        internal async Task CreateTopicAsync()
+        {
+            using var adminClient = new AdminClientBuilder(new AdminClientConfig()
+            {
+                BootstrapServers = "localhost:9094"
+            }).Build();
+
+            try
+            {
+                await adminClient.CreateTopicsAsync(new[]
+                {
+                    new TopicSpecification() { Name = TopicName, NumPartitions = 3, ReplicationFactor = 1 }
+                });
+
+                Console.WriteLine($"Topic({TopicName}) oluştu.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- Kafka.Producer/Program.cs
+++ Kafka.Producer/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Kafka.Producer;
+
 Console.WriteLine("Kafka Producer");
+
+
+var kafkaService = new KafkaService();
+
+await kafkaService.CreateTopicAsync();
