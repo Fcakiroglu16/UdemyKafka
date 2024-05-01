3099f5db2b262109040ad68a6915d272a8d89728
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-05-01 16:03:32 +0300

    Producer  ( Use case - 1)

--- Kafka.Producer/KafkaService.cs
+++ Kafka.Producer/KafkaService.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Confluent.Kafka;
+﻿using Confluent.Kafka;
 using Confluent.Kafka.Admin;
 
 namespace Kafka.Producer
 {
     internal class KafkaService
     {
-        private const string TopicName = "mytopic";
-
-
-        internal async Task CreateTopicAsync()
+        internal async Task CreateTopicAsync(string topicName)
         {
             using var adminClient = new AdminClientBuilder(new AdminClientConfig()
             {
@@ -24,14 +16,42 @@ namespace Kafka.Producer
             {
                 await adminClient.CreateTopicsAsync(new[]
                 {
-                    new TopicSpecification() { Name = TopicName, NumPartitions = 3, ReplicationFactor = 1 }
+                    new TopicSpecification() { Name = topicName, NumPartitions = 3, ReplicationFactor = 1 }
                 });
 
-                Console.WriteLine($"Topic({TopicName}) oluştu.");
+                Console.WriteLine($"Topic({topicName}) oluştu.");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+            }
+        }
+
+
+        internal async Task SendSimpleMessageWithNullKey(string topicName)
+        {
+            var config = new ProducerConfig() { BootstrapServers = "localhost:9094" };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+
+            foreach (var item in Enumerable.Range(1, 10))
+            {
+                var message = new Message<Null, string>()
+                {
+                    Value = $"Message(use case -1) - {item}"
+                };
+
+                var result = await producer.ProduceAsync(topicName, message);
+
+
+                foreach (var propertyInfo in result.GetType().GetProperties())
+                {
+                    Console.WriteLine($"{propertyInfo.Name} : {propertyInfo.GetValue(result)}");
+                }
+
+                Console.WriteLine("-----------------------------------");
+                await Task.Delay(200);
             }
         }
     }
--- Kafka.Producer/Program.cs
+++ Kafka.Producer/Program.cs
@@ -8,5 +8,9 @@ Console.WriteLine("Kafka Producer");
 
 
 var kafkaService = new KafkaService();
+var topicName = "use-case-1-topic";
+await kafkaService.CreateTopicAsync(topicName);
+await kafkaService.SendSimpleMessageWithNullKey(topicName);
 
-await kafkaService.CreateTopicAsync();
+
+Console.WriteLine("Mesajlar gönderilmiştir.");
