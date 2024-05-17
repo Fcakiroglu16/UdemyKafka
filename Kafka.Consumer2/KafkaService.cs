554d1df0ac71610354f96b9102bd85460815ba48
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-05-17 16:09:32 +0300

    bug fixed

---
+++ Kafka.Consumer2/KafkaService.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+
+namespace Kafka.Consumer2
+{
+    internal class KafkaService
+    {
+        internal async Task ConsumeSimpleMessageWithNullKey(string topicName)
+        {
+            var config = new ConsumerConfig()
+            {
+                BootstrapServers = "localhost:9094",
+                GroupId = "use-case-1-group-2",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            var consumer = new ConsumerBuilder<Null, string>(config).Build();
+            consumer.Subscribe(topicName);
+
+            while (true)
+            {
+                var consumeResult = consumer.Consume(5000);
+
+                if (consumeResult != null)
+                {
+                    Console.WriteLine($"gelen mesaj : {consumeResult.Message.Value}");
+                }
+
+                await Task.Delay(500);
+            }
+        }
+    }
+}
---
+++ Kafka.Consumer2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Kafka.Consumer2;
+
+Console.WriteLine("Kafka Consumer 2");
+var topicName = "use-case-1.1-topic";
+var kafkaService = new KafkaService();
+
+await kafkaService.ConsumeSimpleMessageWithNullKey(topicName);
+
+Console.ReadLine();
